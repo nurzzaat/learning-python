8358c4a51fea5ca92b15036370c9025cc98a84fd
Nurzat Tynyshbekov <xxi.le.77@gmail.com>
2024-11-28 02:13:07 +0500

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,59 @@
+#1 task
+width = 10
+height = 20
+area = width * height
+print(area)
+
+roomArea = area / 2
+print(roomArea)
+
+perimeter = (2 * (height + width))
+print(perimeter)
+
+#2 task
+print(12 % 5)
+
+#3 task
+heartRate1 = 70
+heartRate2 = 80
+heartRate3 = 90
+
+addedHR = heartRate1 + heartRate2 + heartRate3
+averageHR = addedHR / 3 
+print(addedHR)
+print(averageHR)
+
+#4 task
+steps = 3467
+goal = 10000
+
+percentOfGoal = (steps * 100) / goal
+print(percentOfGoal)
+
+#5 task
+balance = 0
+print(balance)
+balance += 10000 
+print(balance)
+balance += 20000
+print(balance)
+balance /= 2 
+print(balance)
+balance *= 3
+print(balance)
+balance -= 3000
+print(balance)
+
+#6 task
+firstNumber = input('Enter the first number: ')
+secondNumber = input('Enter the second number: ')
+print(int(firstNumber) // int(secondNumber))
+
+#7 task
+a = 5
+b = 10
+c = 3 
+result = a + b * c / (a - 1) ** 2
+print(result)
+result = (a + b) * c / (a - 1) ** 2
+print(result)
