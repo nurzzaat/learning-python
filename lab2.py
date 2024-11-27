68f97e861f70cafa3e78b084ff04a252c229bc6e
Nurzat Tynyshbekov <xxi.le.77@gmail.com>
2024-11-28 01:56:34 +0500

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,33 @@
+'''The first task'''
+goalSteps = 1000
+print(goalSteps)
+
+#The second task
+schooling = 11 #I studied 11 years at school
+print(schooling)
+schooling = 12 #For NIS students
+print(schooling)
+
+#The third task
+steps = 0
+print(steps)
+steps = 2000
+print(steps)
+print("Жақсы жұмыс! Сіз өзіңіздің күнделікті мақсатыңызға жетіп жатырсыз")
+
+#The fourth task
+distanceTraveled = 0
+distanceTraveled = 54.3
+print(distanceTraveled)
+
+#5 task
+isHungry = False
+
+#6 task
+bigInteger = 5_678_910_000
+
+#7 task
+breakTime = 15
+print(breakTime)
+breakTime = 15
+print(breakTime)
